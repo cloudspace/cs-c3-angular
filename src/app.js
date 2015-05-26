bfb866789b23620bf4290cda21c0e1dc43b450d5
Christopher McLean <havok2905@gmail.com>
2015-05-26 15:21:30 -0400

    project init

---
+++ src/app.js
@@ -0,0 +1,57 @@
+/*
+  EXAMPLE USE
+
+  <div cs-c-three
+       id='container'
+       container='"#container"'
+       columns='csC3Ctrl.columns'
+       axes='csC3Ctrl.axes'
+       colors='csC3Ctrl.colors'
+       type='"spline"'
+       legend='true'
+       ytwo='true'>
+  </div>
+ */
+
+module.exports = angular
+  .module('csC3', [])
+  .directive('csCThree', ['csCThree', function(csCThree) {
+    return {
+      restict: 'A',
+      scope: {
+        columns:   '&',
+        axes:      '&',
+        container: '&',
+        type:      '&',
+        legend:    '&',
+        colors:    '&',
+        ytwo:      '&'
+      },
+      bindToController: true,
+      controllerAs: 'csC3',
+      controller: [function() {
+
+      }],
+      link: function(scope, element, attr, ctrl) {
+
+        ctrl.chart = c3.generate({
+          bindto: ctrl.container(),
+          data: {
+            columns: ctrl.columns(),
+            axes:    ctrl.axes(),
+            type:    ctrl.type(),
+            colors:  ctrl.colors()
+          },
+          axis: {
+            y2: {
+              show: ctrl.ytwo()
+            }
+          },
+          legend: {
+            show: ctrl.legend()
+          }
+        });
+
+      }
+    };
+  }]);
